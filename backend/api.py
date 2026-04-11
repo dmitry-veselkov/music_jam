21db56cae46eb5fce50777741c60ed2dd5ad3fbe
Alex <sas.spirin2006@gmail.com>
2026-04-11 18:57:40 +0500

    Rewrite backend base to OOP

---
+++ backend/api.py
@@ -0,0 +1,47 @@
+﻿from flask import request, jsonify
+
+
+class Api:
+    def __init__(self, app):
+        self.app = app
+        self.teams = {
+            "fake-uuid-1": "БобрДобр",
+            "fake-uuid-2": "Недавно гипербола"
+        }
+        # TODO избавиться от self.teams как простой dict
+        # мб передавать БД в качестве аргумента для запросов
+
+    def get_room_info(self):
+        code = request.args.get('roomCode').upper()
+
+        # TODO Переделать MOCK
+        # TODO Переписать self.teams на запросы к БД
+        exists = code == "12345"
+        room_info = {
+            "status": "waiting",
+            "gameName": "Хиты 80-х",
+            "creator": "Очень добрый человек",
+            "teams": list(self.teams.values())
+        } if exists else None
+
+        return jsonify({"roomCode": code, "exists": exists, "roomInfo": room_info})
+
+    def set_team_name(self):
+        data = request.get_json()
+        team_id = data.get('teamId')
+
+        is_new_team = team_id not in self.teams
+        old_name = self.teams.get(team_id)
+
+        self.teams[data.get('teamId')] = data.get('newName')
+        return jsonify({
+            "status": "ok",
+            "isNewTeam": is_new_team,
+            "oldName": old_name,
+            "newName": data.get('newName')
+        })
+
+    def get_team_name(self):
+        data = request.get_json()
+        team_name = self.teams.get(data.get('uuid'))
+        return jsonify({"teamName": team_name})
---
+++ backend/app.py
@@ -0,0 +1,32 @@
+﻿from flask import Flask, send_from_directory, jsonify, request
+import pathlib
+
+from backend.api import Api
+
+
+class App:
+    BASE_DIR = pathlib.Path(__file__).parent.parent
+    BACKEND_DIR = BASE_DIR / 'backend'
+    FRONTEND_DIR = BASE_DIR / 'frontend'
+
+    def __init__(self):
+        self.app = Flask(__name__, static_folder=str(self.FRONTEND_DIR))
+        self.api = Api(self.app)
+
+        self.app.add_url_rule('/', 'index', self.index, defaults={'path': ''})
+        self.app.add_url_rule('/<path:path>', 'index', self.index)
+        self.app.add_url_rule('/api/check_room', None, self.api.get_room_info, methods=['GET'])
+        self.app.add_url_rule('/api/set_team_name', None, self.api.set_team_name, methods=['POST'])
+        self.app.add_url_rule('/api/get_team_name', None, self.api.get_team_name, methods=['POST']) # сделать бы на GET обычный, но тут хз
+
+    def run(self, **kwargs):
+        self.app.run(**kwargs)
+
+    def index(self, path):
+        if path != '' and pathlib.Path(str(self.app.static_folder)).joinpath(path).exists():
+            return send_from_directory(str(self.app.static_folder), path)
+        return send_from_directory(str(self.app.static_folder), 'index.html')
+
+
+if __name__ == "__main__":
+    App().run(debug=True, port=5000)
