ea9a36dd1827b55b59be7e94e49f442712d48430
Alex <sas.spirin2006@gmail.com>
2026-04-12 16:20:04 +0500

    Fix .pre-commit-config.yaml & refactor api and app

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,7 @@
-﻿from flask import Flask, send_from_directory, jsonify, request
 import pathlib
+from typing import Any
+
+from flask import Flask, Response, send_from_directory
 
 from backend.api import Api
 
@@ -9,20 +11,21 @@ class App:
     BACKEND_DIR = BASE_DIR / 'backend'
     FRONTEND_DIR = BASE_DIR / 'frontend'
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.app = Flask(__name__, static_folder=str(self.FRONTEND_DIR))
         self.api = Api(self.app)
 
-        self.app.add_url_rule('/', 'index', self.index, defaults={'path': ''})
-        self.app.add_url_rule('/<path:path>', 'index', self.index)
-        self.app.add_url_rule('/api/check_room', None, self.api.get_room_info, methods=['GET'])
-        self.app.add_url_rule('/api/set_team_name', None, self.api.set_team_name, methods=['POST'])
-        self.app.add_url_rule('/api/get_team_name', None, self.api.get_team_name, methods=['POST']) # сделать бы на GET обычный, но тут хз
+        self.app.add_url_rule('/', view_func=self.index, defaults={'path': ''})
+        self.app.add_url_rule('/<path:path>', view_func=self.index)
+        self.app.add_url_rule('/api/check_room', view_func=self.api.get_room_info, methods=['GET'])
+        self.app.add_url_rule('/api/set_team_name', view_func=self.api.set_team_name, methods=['POST'])
+        self.app.add_url_rule('/api/get_team_name', view_func=self.api.get_team_name, methods=['POST'])
+        # сделать бы на GET обычный, но тут хз
 
-    def run(self, **kwargs):
+    def run(self, **kwargs: Any) -> None:
         self.app.run(**kwargs)
 
-    def index(self, path):
+    def index(self, path: str) -> Response:
         if path != '' and pathlib.Path(str(self.app.static_folder)).joinpath(path).exists():
             return send_from_directory(str(self.app.static_folder), path)
         return send_from_directory(str(self.app.static_folder), 'index.html')
