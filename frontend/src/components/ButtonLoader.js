aeb691eff1e83074dbd2d2b974adb94a415e7cd7
Dmitry Veselkov <d.veselkov2017@yandex.ru>
2026-04-23 21:17:08 +0500

    начало рефакторинга. спиннер загрузки, текст об ошибке и порядок в классах авторизации

---
+++ frontend/src/components/ButtonLoader.js
@@ -0,0 +1,47 @@
+﻿export class LoadingButton {
+    /**
+     * Класс для управления кнопкой с лоудером
+     */
+
+    static html = `
+        <button class="loading-btn">
+            <span class="btn-text"></span>
+            <span class="btn-spinner"></span>
+        </button>
+    `;
+
+    constructor(container) {
+        this.container = container;
+        this.button = this.container.querySelector('.loading-btn');
+        this.textEl = this.container.querySelector('.btn-text');
+        this.spinner = this.container.querySelector('.btn-spinner');
+
+        this.isLoading = false;
+    }
+
+    setText(text) {
+        this.textEl.textContent = text;
+    }
+
+    startLoading() {
+        /**
+         * Включаем состояние загрузки
+         */
+        if (this.isLoading) return;
+
+        this.isLoading = true;
+
+        this.button.classList.add('loading');
+        this.button.disabled = true;
+    }
+
+    stopLoading() {
+        /**
+         * Выключаем состояние загрузки
+         */
+        this.isLoading = false;
+
+        this.button.classList.remove('loading');
+        this.button.disabled = false;
+    }
+}
---
+++ frontend/src/components/Error.js
@@ -0,0 +1,27 @@
+﻿export class WrongText {
+    /**
+     * Класс для вывода сообщения об ошибке
+     */
+
+    static html = `<p class="wrong-text"></p>`;
+
+    constructor(container) {
+        this.container = container;
+        this.wrongText = this.container.querySelector('.wrong-text');
+    }
+
+    showWrongText(msg) {
+        /**
+         * Отображаем текст об ошибке
+         */
+        this.wrongText.style.display = 'block';
+        this.wrongText.textContent = msg;
+    }
+
+    hideWrongText() {
+        /**
+         * Скрываем текст об ошибке
+         */
+        this.wrongText.style.display = 'none';
+    }
+}
